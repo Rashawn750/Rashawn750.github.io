d35f7c2f4dbd49163f8e293e5da93a45a70214ad
Rashawn750 <rashawn.freeman.student@madisonpsb.org>
2021-10-21 15:43:33 +0000

    platform

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,8 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(500, 100);
-        createPlatform(170, 250,);
+        createPlatform(500, 100); //platform 1
+        createPlatform();
         createPlatform();
         createPlatform();
         createPlatform()
